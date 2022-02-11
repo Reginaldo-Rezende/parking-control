981a837a993aedbb6d46a5ab96f0a7667c3d60e0
Reginaldo-Rezende <Reginaldors777@hotmail.com>
2022-02-11 13:46:03 -0300

    interface ajustada

--- src/main/java/com/api/parkingcontrol/repositories/VagaDoCarroRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/VagaDoCarroRepository.java
@@ -2,9 +2,11 @@ package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.VagaDoCarroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface VagaDoCarroRepository extends JpaRepository<VagaDoCarroModel, UUID> {
 
 
